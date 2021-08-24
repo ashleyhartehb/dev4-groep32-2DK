da06bd3bc37b876ec5d10f1ee6b7ec0f8e65752f
Ashley J. Hart <ashley.hart@student.ehb.be>
2021-08-24 17:27:21 +0200

    Starten met js file => Command design pattern aangepast aan javascript. Calculate, Command, AddCommand, SubtractCommand en CommandsController objecten werden aangemaakt. HTML werd aangepast aan content => buttons gemaakt om alles te kunnen testen. Zit gewoon nog vast aan command.execute. Normaal gezien is AddCommand een command object dus zou .execute aangeroepen moeten worden via constructor..

--- Dev4_groep32/Javascript/Command.js
+++ Dev4_groep32/Javascript/Command.js
@@ -3,60 +3,101 @@ we basically only have impure code because our application mostly relies on data
 and each command shares that data globally so we have alot of side effects 
 */
 
-let Command = function (execute, value) {
+/* Code gebaseerd op https://www.dofactory.com/javascript/design-patterns/command */
+let totalText = document.getElementById("total");
+let total = 0.0;
+
+
+let Calculate = function () {
+
+  return {
+    add: function (price) {
+      console.log(typeof total);
+      total += price;
+      totalText.innerText = `${total}`
+      console.log(total);
+      return total;
+    },
+    subtract: function (price) {
+      total -= price
+      totalText.innerText = `${total}`
+      console.log(total);
+      return total;
+    }
+  }
+}
+
+let calculate = new Calculate();
+
+
+let Command = function (execute, undo, price) {
   this.execute = execute
-  this.value = User 
-}
-
-let CreateCommand = function (value) {
-  username = prompt("Choose a username")
-  firstName = prompt("What's your first name?")
-  lastName = prompt("What's your last name?")
-  age = prompt("How old are you?")
-  phone = prompt("What's your phone number?")
-  value = new User(username, firstName, lastName, age, email, phone, interests)
-  return new Command(value)
+  this.undo = undo;
+  this.price = price;
 
 }
 
-let EditCommand = function (value) {
-  username = prompt("Choose another username")
-  firstName = prompt("Write another first name")
-  lastName = prompt("Write another last name")
-  age = prompt("Write another age")
-  phone = prompt("Write another phone number")
-  value = new User(username, firstName, lastName, age, email, phone, interests)
-  
-  return new Command(value)
+let AddCommand = function (price) {
+  let add = calculate.add(price);
+  let subtract = calculate.subtract(price);
+  return new Command(add, subtract, price);
 }
 
-/*
-let DeleteCommand = function (value) {
-  return new Command(value)
+let SubCommand = function (price) {
+  let add = calculate.add(price);
+  let subtract = calculate.subtract(price);
+  return new Command(subtract, add, price);
 }
-*/
 
-let ShowCommand = function (value) {
-  username = document.getElementById("username")
-  firstName = document.getElementById("firstName")
-  lastName = document.getElementById("lastName")
-  age = document.getElementById("age")
-  email = document.getElementById("email")
-  phone = document.getElementById("phone")
-  interests = document.getElementById("interests")
 
-  username.innerHTML = value.username
-  firstName.innerHTML = value.firstName
-  lastName.innerHTML = value.lastName
-  age.innerHTML = value.age
-  email.innerHTML = value.email
-  phone.innerHTML = value.phone
-  interests.innerHTML = value.interests.forEach(element => {
-    `<li><button class="delete"> X </button> ${element} </li>`
-  })
-  return new Command(value)
+let CommandsController = function () {
+  let currentValue = 0;
+  let commands = [];
+
+  function action(command) {
+    var name = command.execute.toString().substr(9, 3);
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+
+  return {
+    execute: function (command) {
+      console.log(command);
+      currentValue = command.execute(currentValue, command.value);
+      commands.push(command);
+      console.log(action(command) + ": " + command.value);
+    },
+
+    undo: function () {
+      console.log(command);
+      var command = commands.pop();
+      currentValue = command.undo(currentValue, command.value);
+      console.log("Undo " + action(command) + ": " + command.value);
+    }
+  }
 }
-command = new Command()
-document.getElementById("create").addEventListener("click",CreateCommand())
-document.getElementById("edit").addEventListener("click", EditCommand)
-//document.getElementsByClassName("delete").addEventListener("click", DeleteCommand)
+
+
+let commandsController = new CommandsController();
+
+//listening to click event of ADDING NUMBER
+let elements = document.getElementsByClassName("add");
+
+for (i = 0; i < elements.length; i++) {
+  let innertext = elements[i].innerText;
+  elements[i].addEventListener("click", () => {
+    let parsedText = parseFloat(innertext)
+    console.log(parsedText);
+    commandsController.execute(new AddCommand(parsedText));
+  });
+}
+
+//listening to click event of UNDOING ACTION
+
+let undoElement = document.getElementById("undo");
+if (undoElement) {
+  undoElement.addEventListener("click", () => {
+    commandsController.undo();
+  });
+} else {
+  console.log(undoElement);
+}
